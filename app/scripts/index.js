bc80fe3f641c8766040f14bb50668734e8c8e845
doobdoob <wuyrush@gmail.com>
2018-12-18 10:59:19 -0800

    Onboard webextension-toolbox to eliminate boilerplate code

---
+++ app/scripts/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+  Input
+} from 'bloomer';
+
+class Ril extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <h1>Ril is cool!</h1>
+    );
+  }
+}
